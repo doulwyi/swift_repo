5c44557ad7cd13179b51568343990a118b7bbbdb
doulwyi <kanazawabruno@gmail.com>
2017-08-04 15:25:26 -0300

    First Commit

---
+++ DogAgeApp/DogAgeApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DogAgeApp
+//
+//  Created by kanazawa on 04/08/17.
+//  Copyright © 2017 kanazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Show a message
+        print("My first App")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
